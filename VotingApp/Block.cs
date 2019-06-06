2738200e836fd4fed8a85e05eb546f8cff09cb81
DonSaik <ds32204@gmail.com>
2019-06-06 13:16:49 +0300

    First upd

---
+++ VotingApp/Block.cs
@@ -0,0 +1,32 @@
+//eddited By Donatas
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace VotingApp {
+
+    [Serializable]
+    public class Block {
+        public int Index { get; set; }
+        public string PreviousHash { get; set; }
+        public string Hash { get; set; }
+        public string Data { get; set; }
+
+        public Block(string previousHash, string data)
+        {
+            Index = 0;
+            PreviousHash = previousHash;
+            Data = data;
+            Hash = CalculateHash();
+        }
+
+        public string CalculateHash()
+        {
+            SHA256 sha256 = SHA256.Create();
+            byte[] HashDataBytes = Encoding.ASCII.GetBytes($"{PreviousHash}-{Data}");
+            byte[] Encryptedhash = sha256.ComputeHash(HashDataBytes);
+            return Convert.ToBase64String(Encryptedhash);
+        }
+    }
+}
